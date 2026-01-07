b768e0c921c14aa61f9e4f1f9e90a4f249e061a2
aqibzafeer <aqibzafeer@users.noreply.github.com>
2026-01-08 00:35:48 +0500

    aaa

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,15 +1,8 @@
-/**
- * Common Components Export
- * Barrel file for easy imports
- */
 
-// New reusable components
 export { default as ProductImage } from "./ProductImage";
 export { default as Section } from "./Section";
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
-
-// Re-export existing components for convenience
 export { default as LoadingSpinner } from "../LoadingSpinner";
 export { default as ProductSkeleton } from "../ProductSkeleton";
 export { default as Pagination } from "../Pagination";
